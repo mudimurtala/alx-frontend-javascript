a9e6e3173fe3614a18ab337544dee94b6dfaffc8
mudimurtala <ibnmuhyideen95@gmail.com>
2024-08-06 05:50:55 +0100

    Add solutions to Task-1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,12 @@
-
 export default function taskBlock(trueOrFalse) {
-    let task = false;
-    const task2 = true;
+    var task = false;
+    var task2 = true;
   
-    if (trueOrFalse) {
-      task = true;
-     
-    }
+  if (trueOrFalse) {
+      var task = true;
+  else
+      var task2 = false;
+  }
   
     return [task, task2];
   }
-  
